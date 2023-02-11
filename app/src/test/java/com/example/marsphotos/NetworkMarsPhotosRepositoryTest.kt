aa96f3492af902abe48fdb4c2e586a6637cde010
Victor Castro <victorcem@hotmail.com>
2023-02-11 11:38:53 -0300

    Test viewmodel and api

---
+++ app/src/test/java/com/example/marsphotos/NetworkMarsPhotosRepositoryTest.kt
@@ -0,0 +1,22 @@
+package com.example.marsphotos
+
+import com.example.marsphotos.data.NetworkMarsPhotosRepository
+import com.example.marsphotos.fake.FakeDataSource
+import com.example.marsphotos.fake.FakeMarsApiService
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class NetworkMarsPhotosRepositoryTest {
+
+    @Test
+    fun networkMarsPhotosRepository_getMarsPhotos_verifyPhotoList() =
+        runTest {
+            val repository = NetworkMarsPhotosRepository(
+                marsApiService = FakeMarsApiService()
+            )
+            assertEquals(FakeDataSource.photosList, repository.getMarsPhotos())
+        }
+}
